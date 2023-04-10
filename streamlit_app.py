41011983179d1b9fcfd139851385be0c9617b1df
Gopi Krishna <38878306+Gopi-Krishna-G@users.noreply.github.com>
2023-04-10 21:41:36 +0530

    Update streamlit_app.py
    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
+import streamlit
+streamlit.title('My parents New Healthy Dinner/Meal')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
